eabd3b30b01825e69dd09da502d0cd719e33f9d2
Cameron James <cjames0815@gmail.com>
2023-09-20 10:58:54 -0400

    StrictlyIdentical

---
+++ main.py
@@ -0,0 +1,37 @@
+from StrictlyIdentical import *
+
+def main():
+    # Sample lists for testing
+    a = ['E', 'B', 'E', 'F', 'A', 'F']
+    first = 0
+    size = 0
+    target = 0 
+    
+    
+    print(__name__)
+
+
+    
+    
+
+
+
+    # Print the two lists
+    print("List 1 and 2:", a,first,size, target)
+    
+   
+
+    
+   
+
+
+    # Print the result
+
+
+    if a:
+        print("Two lists are strictly identical!")
+    else:
+        print("Two lists aren't strictly identical!")
+
+if __name__ == "__main__":
+    main()
